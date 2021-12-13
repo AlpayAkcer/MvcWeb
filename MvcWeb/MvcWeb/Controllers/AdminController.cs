bf5520a41796848e52b02fb49a64fd3ef4dd0d35
Alpay Akçer <info@alpayakcer.com>
2021-12-13 17:54:08 +0300

    User picture hatası düzeltildi

--- MvcWeb/MvcWeb/Controllers/AdminController.cs
+++ MvcWeb/MvcWeb/Controllers/AdminController.cs
@@ -14,6 +14,11 @@ namespace MvcWeb.Controllers
     {
         Context db = new Context();
 
+        public ActionResult Index()
+        {
+            var value = db.Admins.Include("UserSocials").ToList();
+            return View(value);
+        }
 
         [HttpGet]
         public ActionResult AddAdmin()
@@ -52,6 +57,68 @@ namespace MvcWeb.Controllers
                 db.Admins.Add(admin);
                 db.SaveChanges();
                 return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            List<SelectListItem> valuerole = (from x in db.Roles
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.RoleName,
+                                                  Value = x.RoleId.ToString()
+                                              }).ToList();
+
+            ViewBag.valuerolelist = valuerole;
+
+            var value = db.Admins.Find(id);
+            return View("EditAdmin", value);
+        }
+
+        [HttpPost]
+        public ActionResult EditAdmin(Admin adm)
+        {
+            AdminValidator validations = new AdminValidator();
+            ValidationResult result = validations.Validate(adm);
+
+            var value = db.Admins.Find(adm.AdminId);
+
+            if (result.IsValid)
+            {
+                if (Request.Files.Count > 0)
+                {
+                    string dosyaAdi = Path.GetFileName(Request.Files[0].FileName);
+                    string yol = "/Uploads/user/" + dosyaAdi;
+                    Request.Files[0].SaveAs(Server.MapPath(yol));
+                    adm.Picture = yol;
+                }
+                else
+                {
+                    adm.Picture = "/Uploads/no_image.png";
+                }
+
+
+                value.UserName = adm.UserName;
+                value.Name = adm.Name;
+                value.Surname = adm.Surname;
+                value.Email = adm.Email;
+                value.RoleId = adm.RoleId;
+                value.Title = adm.Title;
+                value.Picture = adm.Picture;
+                value.Password = adm.Password;
+                db.SaveChanges();
+                TempData["AlertMessage"] = "Admin Başarıyla Güncellendi";
+                //c.Entry(d).State = EntityState.Modified;
+                return RedirectToAction("Index");
             }
             else
             {
